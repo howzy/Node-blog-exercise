a3b50b40e9e1f03423374c2f70cee538f8e2c5de
once <zhonce829@gmail.com>
2017-07-09 16:31:48 +0800

    fix spelling mistake

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var app = express();
 // 设置模板目录
 app.set('views', path.join(__dirname, 'views'));
 // 设置模板引擎为 ejs
-app.set('views engine', 'ejs');
+app.set('view engine', 'ejs');
 
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ module.exports = function (app) {
   });
   app.use('/signup', require('./signup'));
   app.use('/signin', require('./signin'));
-  app.use('/singout', require('./signout'));
+  app.use('/signout', require('./signout'));
   app.use('/posts', require('./posts'));
 };
